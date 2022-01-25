b5d902e797b18a8fb03a3f9d3aa19cdecd4b1d4b
leon.zhou <zhouleib1412@gmail.com>
2022-01-25 13:42:04 +1300

    init

---
+++ tool.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+nvm_version=0.39.1
+
+## xcode
+sudo xcodebuild -license
+
+## install brew
+if ! command -v brew &> /dev/null
+then
+  echo 'Brew is not installed. Installing'
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+if ! command -v sdk &> /dev/null
+then
+  echo 'Sdk is not installed. Installing'
+  curl -s "https://get.sdkman.io" | bash
+  source "$HOME/.sdkman/bin/sdkman-init.sh"
+fi
+
+## install java-related tools via sdkman
+sdk install maven
+
+brew update
+brew tap homebrew/cask-versions
+brew tap ringohub/redis-cli
+
+# utilities
+
+for value in etcd protobuf grpcurl go git git-gui awscli tree kubernetes-cli kubernetes-helm yarn packer wget httpie vim zsh make redis-cli mysql-client
+do
+  if ! [ -x "$(command -v $value)" ];
+  then
+    echo $value 'is not installed. Installing'
+    brew install $value
+  fi
+done
+
+
+## oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## nvm
+if ! command -v nvm &> /dev/null
+then
+  echo 'nvm is not installed. Installing'
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v$nvm_version/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  nvm install node
+fi
