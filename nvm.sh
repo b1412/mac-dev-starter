5e7cc0f5e5331255d1781110c7107e7f8fec23f0
leon.zhou <zhouleib1412@gmail.com>
2022-01-25 14:52:55 +1300

    update

--- nvm.sh
+++ nvm.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-nvm_version=
+nvm_version=v0.39.1
 
 ## nvm
 if ! command -v nvm &> /dev/null
 then
   echo 'nvm is not installed. Installing'
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$nvm_version/install.sh | bash
   source ~/.zshrc
   nvm install node
 fi
--- tool.sh
+++ tool.sh
@@ -42,3 +42,5 @@ done
 ## oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+## nvm
+./nvm.sh
