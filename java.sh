d8a5871260f5e77b144e13e9f388b9a83953d5c5
leon.zhou <zhouleib1412@gmail.com>
2022-01-26 13:43:27 +1300

    update

--- java.sh
+++ java.sh
@@ -5,6 +5,8 @@ echo 'eval "$(jenv init -)"' >> ~/.zshrc
 brew install adoptopenjdk/openjdk/adoptopenjdk8
 brew install adoptopenjdk/openjdk/adoptopenjdk11
 brew install adoptopenjdk/openjdk/adoptopenjdk14
+echo "listing all SDK"
+ls /Library/Java/JavaVirtualMachines/
 jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/
 jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/
 jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-14.jdk/Contents/Home/
