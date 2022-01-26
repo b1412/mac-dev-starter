2a53d7f579580939942fd926e94628699947a884
leon.zhou <zhouleib1412@gmail.com>
2022-01-26 13:07:17 +1300

    update java

--- java.sh
+++ java.sh
@@ -2,7 +2,7 @@ brew tap homebrew/cask-versions
 brew install jenv
 echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(jenv init -)"' >> ~/.zshrc
-brew install --cask adoptopenjdk11
+brew install adoptopenjdk/openjdk/adoptopenjdk11
 jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/
 jenv versions
 jenv global 11
